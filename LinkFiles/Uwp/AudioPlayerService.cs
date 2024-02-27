230231e1f3c1ca633f7b0824eb657d96529e4bc4
peterrexj <peterrexj@hotmail.com>
2024-02-28 00:26:39 +1100

    fix uwp device specific

--- LinkFiles/Uwp/AudioPlayerService.cs
+++ LinkFiles/Uwp/AudioPlayerService.cs
@@ -1,51 +1,127 @@
-﻿//using Prayers.Services;
-//using Prayers.UWP.Services;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Windows.ApplicationModel;
-//using Windows.Media.Core;
-//using Windows.Media.Playback;
-//using Windows.Storage;
-//using Windows.UI.Xaml.Controls;
-//using Xamarin.Essentials;
-//using Xamarin.Forms;
+﻿using Microsoft.AppCenter.Crashes;
+using Pj.Library;
+using Prayers.Services;
+using Prayers.UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Forms;
 
-//[assembly: Dependency(typeof(AudioPlayerService))]
-//namespace Prayers.UWP.Services
-//{
-//    public class AudioPlayerService : IAudioPlayerService
-//    {
-//        private MediaPlayer mediaPlayer;
+[assembly: Dependency(typeof(AudioPlayerService))]
+namespace Prayers.UWP.Services
+{
+    public class AudioPlayerService : IAudioPlayerService
+    {
+        private MediaPlayer _mediaPlayer;
+        private Queue<string> _mediaFilesToPlay;
 
-//        public async Task Play(string audioFilePath)
-//        {
-//            StorageFolder Folder = await Package.Current.InstalledLocation.GetFolderAsync(@"Sounds");
-//            StorageFile sf = await Folder.GetFileAsync(audioFilePath);
-//            var PlayMusic = new MediaElement();
-//            PlayMusic.AudioCategory = Windows.UI.Xaml.Media.AudioCategory.Media;
-//            PlayMusic.SetSource(await sf.OpenAsync(FileAccessMode.Read), sf.ContentType);
-//            PlayMusic.Play();
-//        }
+        public AudioPlayerService()
+        {
+            _mediaFilesToPlay = new Queue<string>();
+        }
 
-//        public void Pause()
-//        {
-//            if (mediaPlayer != null && mediaPlayer.PlaybackSession.PlaybackState == MediaPlaybackState.Playing)
-//            {
-//                mediaPlayer.Pause();
-//            }
-//        }
+        private void InitializePlayer()
+        {
+            _mediaPlayer = new MediaPlayer();
+            _mediaPlayer.MediaEnded += _mediaPlayer_MediaEnded;
+        }
+        private void ReleasePlayer()
+        {
+            _mediaPlayer.MediaEnded -= _mediaPlayer_MediaEnded;
+            _mediaPlayer.Source = null;
+            _mediaPlayer = null;
+        }
 
-//        public void Stop()
-//        {
-//            if (mediaPlayer != null)
-//            {
-//                mediaPlayer.Pause();
-//                mediaPlayer.Source = null;
-//                mediaPlayer = null;
-//            }
-//        }
-//    }
-//}
+        private async Task CheckAudioToPlay()
+        {
+            if (_mediaFilesToPlay.Count <= 0)
+            {
+                SharedServices.AudioController.AudioComplete();
+                return;
+            }
+            var file = _mediaFilesToPlay.Dequeue();
+            if (file.IsEmpty()) return;
+
+            InitializePlayer();
+            Uri uri = new Uri($"ms-appx:///Assets/{file}", UriKind.Absolute);
+            StorageFile localStorageFile = await StorageFile.GetFileFromApplicationUriAsync(uri);
+            if (localStorageFile != null)
+            {
+                var stream = await localStorageFile.OpenAsync(FileAccessMode.Read);
+                _mediaPlayer.Source = MediaSource.CreateFromStream(stream, localStorageFile.ContentType);
+                _mediaPlayer.Play();
+            }
+        }
+        private async void _mediaPlayer_MediaEnded(MediaPlayer sender, object args)
+        {
+            try
+            {
+                ReleasePlayer();
+                await CheckAudioToPlay();
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+
+        public bool IsMediaPlaying() => _mediaPlayer?.PlaybackSession.PlaybackState == MediaPlaybackState.Playing;
+
+        public async Task Play(List<string> pathToAudio)
+        {
+            try
+            {
+                if (_mediaPlayer != null && IsMediaPlaying()) return;
+                if (_mediaPlayer != null && IsMediaPlaying() == false)
+                {
+                    _mediaPlayer.Play();
+                }
+                else
+                {
+                    _mediaFilesToPlay.Clear();
+                    pathToAudio.Iter(f => _mediaFilesToPlay.Enqueue(f));
+                    await CheckAudioToPlay();
+                }
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+        public void Pause()
+        {
+            try
+            {
+                if (_mediaPlayer != null && IsMediaPlaying())
+                {
+                    _mediaPlayer.Pause();
+                }
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                if (_mediaPlayer != null)
+                {
+                    _mediaPlayer.Pause();
+                    ReleasePlayer();
+                }
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+    }
+}
