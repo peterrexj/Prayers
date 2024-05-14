575c0479debe527458a3449ad53e9ed32b705053
peterrexj <peterrexj@hotmail.com>
2024-05-14 11:13:40 +1000

    adding MAUI project

---
+++ src/Prayer.StJoseph.Malayalam/Prayer.StJoseph.Malayalam/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Prayer.StJoseph.Malayalam
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ src/Prayer.StJoseph.Malayalam/Prayers.CoreUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Prayer.CoreUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
